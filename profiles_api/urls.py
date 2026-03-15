11512d8feb4623462f021963650da97b8abb2eab
Senanur <sena.bayram3269@gmail.com>
2026-03-15 21:51:32 +0300

    Added user profile

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-
 from profiles_api import views
+from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
+    path('login/', views.UserLoginApiView.as_view()),
 ]
